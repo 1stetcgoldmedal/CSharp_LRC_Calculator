edea278b9f4d80f863c55de6033a2e57af024b56
1stetcgoldmedal <wlghks7632@gmail.com>
2023-04-20 09:10:12 +0900

    Fist Init

---
+++ LRC_Calculator/Main.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace LRC_Calculator
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputtextBox = new System.Windows.Forms.TextBox();
+            this.IASCIIradioButton = new System.Windows.Forms.RadioButton();
+            this.IHexradioButton = new System.Windows.Forms.RadioButton();
+            this.Inputpanel = new System.Windows.Forms.Panel();
+            this.Outputpanel = new System.Windows.Forms.Panel();
+            this.OASCIIradioButton = new System.Windows.Forms.RadioButton();
+            this.OHexradioButton = new System.Windows.Forms.RadioButton();
+            this.OutputtextBox = new System.Windows.Forms.TextBox();
+            this.Inputpanel.SuspendLayout();
+            this.Outputpanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // InputtextBox
+            // 
+            this.InputtextBox.Location = new System.Drawing.Point(3, 3);
+            this.InputtextBox.Name = "InputtextBox";
+            this.InputtextBox.Size = new System.Drawing.Size(375, 21);
+            this.InputtextBox.TabIndex = 0;
+            this.InputtextBox.TextChanged += new System.EventHandler(this.InputtextBox_TextChanged);
+            // 
+            // IASCIIradioButton
+            // 
+            this.IASCIIradioButton.AutoSize = true;
+            this.IASCIIradioButton.Location = new System.Drawing.Point(3, 30);
+            this.IASCIIradioButton.Name = "IASCIIradioButton";
+            this.IASCIIradioButton.Size = new System.Drawing.Size(54, 16);
+            this.IASCIIradioButton.TabIndex = 2;
+            this.IASCIIradioButton.Text = "ASCII";
+            this.IASCIIradioButton.UseVisualStyleBackColor = true;
+            // 
+            // IHexradioButton
+            // 
+            this.IHexradioButton.AutoSize = true;
+            this.IHexradioButton.Checked = true;
+            this.IHexradioButton.Location = new System.Drawing.Point(63, 30);
+            this.IHexradioButton.Name = "IHexradioButton";
+            this.IHexradioButton.Size = new System.Drawing.Size(45, 16);
+            this.IHexradioButton.TabIndex = 3;
+            this.IHexradioButton.TabStop = true;
+            this.IHexradioButton.Text = "Hex";
+            this.IHexradioButton.UseVisualStyleBackColor = true;
+            // 
+            // Inputpanel
+            // 
+            this.Inputpanel.Controls.Add(this.IASCIIradioButton);
+            this.Inputpanel.Controls.Add(this.IHexradioButton);
+            this.Inputpanel.Controls.Add(this.InputtextBox);
+            this.Inputpanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Inputpanel.Location = new System.Drawing.Point(0, 0);
+            this.Inputpanel.Name = "Inputpanel";
+            this.Inputpanel.Size = new System.Drawing.Size(381, 56);
+            this.Inputpanel.TabIndex = 4;
+            // 
+            // Outputpanel
+            // 
+            this.Outputpanel.Controls.Add(this.OASCIIradioButton);
+            this.Outputpanel.Controls.Add(this.OHexradioButton);
+            this.Outputpanel.Controls.Add(this.OutputtextBox);
+            this.Outputpanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Outputpanel.Location = new System.Drawing.Point(0, 56);
+            this.Outputpanel.Name = "Outputpanel";
+            this.Outputpanel.Size = new System.Drawing.Size(381, 58);
+            this.Outputpanel.TabIndex = 5;
+            // 
+            // OASCIIradioButton
+            // 
+            this.OASCIIradioButton.AutoSize = true;
+            this.OASCIIradioButton.Location = new System.Drawing.Point(3, 30);
+            this.OASCIIradioButton.Name = "OASCIIradioButton";
+            this.OASCIIradioButton.Size = new System.Drawing.Size(54, 16);
+            this.OASCIIradioButton.TabIndex = 2;
+            this.OASCIIradioButton.Text = "ASCII";
+            this.OASCIIradioButton.UseVisualStyleBackColor = true;
+            this.OASCIIradioButton.CheckedChanged += new System.EventHandler(this.OASCIIradioButton_CheckedChanged);
+            // 
+            // OHexradioButton
+            // 
+            this.OHexradioButton.AutoSize = true;
+            this.OHexradioButton.Checked = true;
+            this.OHexradioButton.Location = new System.Drawing.Point(63, 30);
+            this.OHexradioButton.Name = "OHexradioButton";
+            this.OHexradioButton.Size = new System.Drawing.Size(45, 16);
+            this.OHexradioButton.TabIndex = 3;
+            this.OHexradioButton.TabStop = true;
+            this.OHexradioButton.Text = "Hex";
+            this.OHexradioButton.UseVisualStyleBackColor = true;
+            this.OHexradioButton.CheckedChanged += new System.EventHandler(this.OHexradioButton_CheckedChanged);
+            // 
+            // OutputtextBox
+            // 
+            this.OutputtextBox.Location = new System.Drawing.Point(3, 3);
+            this.OutputtextBox.Name = "OutputtextBox";
+            this.OutputtextBox.Size = new System.Drawing.Size(375, 21);
+            this.OutputtextBox.TabIndex = 0;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 114);
+            this.Controls.Add(this.Outputpanel);
+            this.Controls.Add(this.Inputpanel);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(397, 153);
+            this.MinimumSize = new System.Drawing.Size(397, 153);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LRC_Calculator";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.Inputpanel.ResumeLayout(false);
+            this.Inputpanel.PerformLayout();
+            this.Outputpanel.ResumeLayout(false);
+            this.Outputpanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputtextBox;
+        private System.Windows.Forms.RadioButton IASCIIradioButton;
+        private System.Windows.Forms.RadioButton IHexradioButton;
+        private System.Windows.Forms.Panel Inputpanel;
+        private System.Windows.Forms.Panel Outputpanel;
+        private System.Windows.Forms.RadioButton OASCIIradioButton;
+        private System.Windows.Forms.RadioButton OHexradioButton;
+        private System.Windows.Forms.TextBox OutputtextBox;
+    }
+}
+
---
+++ LRC_Calculator/Main.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LRC_Calculator
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 바이트 배열에 대한 LRC값을 구함. 모든 바이트 배열의 값을 더한후 하위 1바이트 값을 가지고
+        /// 비트 반전 한 값에 +1 을 한뒤 리턴
+        /// </summary>
+        /// <param name="tmp"></param>
+        /// <returns></returns>
+        private byte Get_LRC(byte[] tmp)
+        {
+
+            int i_tmp = 0;
+            foreach(byte x in tmp)
+            {
+                i_tmp += x;
+            }
+            i_tmp %= 256;
+            i_tmp = ~i_tmp + 1;
+            return BitConverter.GetBytes(i_tmp)[0];
+        }
+
+        private byte[] string_to_byte(string tmp)
+        {
+            if (tmp.Length % 2 == 1)
+            {
+                return null;
+            }
+            byte[] bytes = new byte[tmp.Length / 2];
+            for (int i = 0; i < tmp.Length; i += 2)
+            {
+                bytes[i / 2] = Convert.ToByte(tmp[i].ToString() + tmp[i + 1].ToString(), 16);
+            }
+            return bytes;
+        }
+
+        private void InputtextBox_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (InputtextBox.Text == "")
+                {
+                    return;
+                }
+                byte rbyte = 0;
+                if (IASCIIradioButton.Checked)
+                {
+                    string tmp = InputtextBox.Text.Replace("\r", "").Replace("\n", "").Replace(":", "");
+                    rbyte = Get_LRC(string_to_byte(tmp));
+
+                    //MessageBox.Show(Get_LRC(Encoding.ASCII.GetBytes(tmp)).ToString());
+                }
+                else if (IHexradioButton.Checked)
+                {
+                    string tmp = InputtextBox.Text.Replace("0x", "").Replace(" ", "");
+                    rbyte = Get_LRC(string_to_byte(tmp));
+                    //Get_LRC(bytes).ToString();정답
+                }
+
+                if (OASCIIradioButton.Checked)
+                {
+                    OutputtextBox.Text = Convert.ToChar(rbyte).ToString();
+                    if (rbyte == 0)
+                    {
+                        OutputtextBox.Text = "NULL 0x00";
+                    }
+                }
+                else if (OHexradioButton.Checked)
+                {
+                    OutputtextBox.Text = BitConverter.ToString(new byte[] { rbyte });
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void OHexradioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            string tmp = InputtextBox.Text;
+            InputtextBox.Text = "";
+            InputtextBox.Text = tmp;
+        }
+
+        private void OASCIIradioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            string tmp = InputtextBox.Text;
+            InputtextBox.Text = "";
+            InputtextBox.Text = tmp;
+        }
+    }
+}
